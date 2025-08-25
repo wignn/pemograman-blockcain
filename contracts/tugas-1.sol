850d11e6d0fb0d91896d8e633fb3716ab00f2268
wignn <tigfiragnafatur1933@gmail.com>
2025-08-25 09:14:35 +0700

    fix

---
+++ contracts/tugas-1.sol
@@ -0,0 +1,16 @@
+pragma solidity 0.8.30;
+
+contract DataStore {
+    struct Person {
+        string nama;
+        string alamat;
+        uint16 tanggal_lahir;
+        string jenis_kelamin;
+    }
+
+    Person[] person;
+
+    function storeData (string memory nama, string memory alamat, uint16 tanggal_lahir, string memory jenis_kelamin) public {
+        person.push(Person(nama, alamat, tanggal_lahir, jenis_kelamin));
+    }
+}
