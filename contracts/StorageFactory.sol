79b4fcf3b90e9a397707172661b9752e7a38f9ac
wignn <tigfiragnafatur1933@gmail.com>
2025-09-01 09:04:11 +0700

    fix

---
+++ contracts/StorageFactory.sol
@@ -0,0 +1,29 @@
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.8.7;
+import "contracts/SimpleStorage.sol";
+
+contract StorageFactory {
+    SimpleStorage[] public SimpleStorageArray;
+
+    function createSimpleStorageContract() public {
+        SimpleStorage simpleStorage = new SimpleStorage();
+        SimpleStorageArray.push(simpleStorage);
+    }
+
+    function sfStorage(
+        uint256 _simpleStorageIndex,
+        uint256 _simpleStorageNumber
+    ) public {
+        SimpleStorage simpleStorage = SimpleStorageArray[_simpleStorageIndex];
+        simpleStorage.store(_simpleStorageNumber);
+    }
+
+    function sfGetStorage(uint256 _simpleStorageIndex)
+        public
+        view
+        returns (uint256)
+    {
+        SimpleStorage simpleStorage = SimpleStorageArray[_simpleStorageIndex];
+        return simpleStorage.retrieve();
+    }
+}
